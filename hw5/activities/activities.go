a7838d03198fcc3ab7b62958f94dbfed689003fa
xfiendx4life <xfiendx4life@gmail.com>
2022-04-28 10:24:23 +0300

    feat: first task

---
+++ hw5/activities/activities.go
@@ -0,0 +1,66 @@
+package activities
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+
+	"github.com/xfiendx4life/gb_back_2_hw/hw5/manager"
+	"github.com/xfiendx4life/gb_back_2_hw/hw5/pool"
+)
+
+type Activity struct {
+	UserId int
+	Date   time.Time
+	Name   string
+}
+
+func (a *Activity) connection(m *manager.Manager, p *pool.Pool) (*sql.DB, error) {
+	s, err := m.ShardById(a.UserId)
+	if err != nil {
+		return nil, err
+	}
+	return p.Connection(s.Address)
+}
+
+func (a *Activity) Create(m *manager.Manager, p *pool.Pool) error {
+	c, err := a.connection(m, p)
+	if err != nil {
+		return err
+	}
+	_, err = c.Exec(`INSERT INTO "activities" VALUES ($1, $2, $3)`, a.UserId,
+		a.Date, a.Name)
+	return err
+}
+func (a *Activity) Read(m *manager.Manager, p *pool.Pool) error {
+	c, err := a.connection(m, p)
+	if err != nil {
+		return err
+	}
+	r := c.QueryRow(`SELECT "date", "name" FROM "activities" WHERE "user_id" =
+	$1`, a.UserId)
+	return r.Scan(
+		&a.Date,
+		&a.Name,
+	)
+}
+func (a *Activity) Update(m *manager.Manager, p *pool.Pool) error {
+	c, err := a.connection(m, p)
+	if err != nil {
+		return err
+	}
+	_, err = c.Exec(`UPDATE "activities" SET "name" = $3, "date" = $4
+	WHERE "user_id" = $1 AND "date" = $2`, a.UserId,
+		a.Name, a.Date, a.Name)
+	return err
+}
+func (a *Activity) Delete(m *manager.Manager, p *pool.Pool) error {
+	c, err := a.connection(m, p)
+	if err != nil {
+		return err
+	}
+	_, err = c.Exec(`DELETE FROM "activities" WHERE "user_id" = $1 AND "date" = $2`,
+		a.UserId, a.Date)
+	return err
+}
--- hw5/cmd/pooler/main.go
+++ hw5/cmd/pooler/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/xfiendx4life/gb_back_2_hw/hw5/activities"
 	"github.com/xfiendx4life/gb_back_2_hw/hw5/manager"
 	"github.com/xfiendx4life/gb_back_2_hw/hw5/pool"
 	"github.com/xfiendx4life/gb_back_2_hw/hw5/user"
@@ -33,6 +35,23 @@ func main() {
 		err := u.Create(m, p)
 		if err != nil {
 			fmt.Println(fmt.Errorf("error on create user %v: %w", u, err))
+		}
+	}
+	acts := []*activities.Activity{
+		{UserId: 1, Name: "election", Date: time.Date(2020, time.November,
+			3, 8, 0, 0, 0, time.FixedZone(time.UTC.String(), -8))},
+		{UserId: 10, Name: "wait at home", Date: time.Date(2020, time.November,
+			3, 8, 0, 0, 0, time.FixedZone(time.UTC.String(), -8))},
+		{UserId: 13, Name: "2021 United States Capitol attack", Date: time.Date(2021, time.January,
+			6, 10, 0, 0, 0, time.FixedZone(time.UTC.String(), -8))},
+		{UserId: 25, Name: "have no idea", Date: time.Date(2021, time.January,
+			6, 10, 0, 0, 0, time.FixedZone(time.UTC.String(), -8))},
+	}
+
+	for _, a := range acts {
+		err := a.Create(m, p)
+		if err != nil {
+			fmt.Println(fmt.Errorf("error on create user %v: %w", a, err))
 		}
 	}
 }
